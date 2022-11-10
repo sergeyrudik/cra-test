588a285c5e166e32e06974e6d5525fca90ab1648
Sergei Rudik <serjik.rudik@gmail.com>
2022-11-10 15:26:50 +0200

    update prj

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <div
             className={'container mx-auto max-w-2xl pt-5'}>
             <Product product={products[0]}/>
+            <Product product={products[1]}/>
         </div>
     )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,13 +4,14 @@ interface ProductProps {
     product: IProduct
 }
 
-export function Product(props: ProductProps) {
-    console.log(props)
+export function Product({product}: ProductProps) {
     return (
         <div
             className='border py-2 px-4 rounded flex flex-col items-center mb-2'
         >
-            {props.product.title}
+            <img src={product.image} className="w-1/6" alt={product.title}/>
+            <p>{product.title}</p>
+            <span className="font-bold">{product.price}</span>
         </div>
     )
 }
