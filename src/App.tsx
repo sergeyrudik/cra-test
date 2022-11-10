cb2b76e0558f8452bafa22d3b4081ad4ed8f11b5
Sergei Rudik <serjik.rudik@gmail.com>
2022-11-10 15:18:53 +0200

    update prj

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Product} from "./components/Product";
+import {products} from "./data/products";
 
 function App() {
-    return(
-        <div className={'container mx-auto max-w-2xl pt-5'}>
-            <Product></Product>
+    return (
+        <div
+            className={'container mx-auto max-w-2xl pt-5'}>
+            <Product product={products[0]}/>
         </div>
     )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,16 @@
-export function Product(){
-    return(
-        <div>
-            Product!
+import {IProduct} from "../models";
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product(props: ProductProps) {
+    console.log(props)
+    return (
+        <div
+            className='border py-2 px-4 rounded flex flex-col items-center mb-2'
+        >
+            {props.product.title}
         </div>
     )
 }
