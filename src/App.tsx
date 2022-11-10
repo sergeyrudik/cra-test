4cb2cefceef53ab554a2209d26d41fb53d2e1a49
Sergei Rudik <serjik.rudik@gmail.com>
2022-11-10 13:00:48 +0200

    update prj

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import {createElement as e} from 'react';
+import {createElement as e, useState} from 'react';
 
 function App() {
+    const [count, setCount] = useState(0)
+
     return e('div', {className: 'container', key: 1}, [
-        e('h1', {className: 'font-bold', key: 2}, 'TEST JSX'),
+        e('h1', {className: 'font-bold', key: 2}, `TEST JSX ${count}`),
         e('button', {
             className: 'py-2 px-4 border',
             key: 3,
-            onClick: () => console.log('Clicked at button!')
+            onClick: () => setCount(count + 1)
         }, 'Click me!'),
     ])
 }
